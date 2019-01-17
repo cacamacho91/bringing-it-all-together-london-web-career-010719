9e5baccc79f64ceeacb0c44b8aa3355fb5fca131
Chris Camacho <cacamacho91@gmail.com>
2019-01-17 15:58:22 +0000

    weird bug

---
+++ lib/dog.rb
@@ -0,0 +1,48 @@
+
+class Dog
+  attr_accessor :id, :name, :breed
+
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      bred TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ? breed = ?
+      WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, name, breed, id)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, name, breed)
+      self.id= DB[:conn].execute("SELECT last_insert_rowid() FROM dogs").flatten.first
+    end
+    self
+  end
+
+end
